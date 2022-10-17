dbdbc79aac47d81bd794bbbeacbc72989dcca007
Shyr <109255166+ShyrcsS@users.noreply.github.com>
2022-10-17 19:29:10 +0700

    segg2vn
    Segggg

---
+++ run.bat
@@ -0,0 +1,13 @@
+@echo off
+title Nahida Seggggg
+cls
+call :LOG [Leak2VN] Welcome
+call :LOG [Leak2VN] To 
+call :LOG [Leak2VN] Segg2VN
+call :LOG [Leak2VN] discord.gg/leak2vn
+
+java -jar grasscutter.jar
+
+:LOG
+echo [%time:~0,8%] %*
+color 0c
